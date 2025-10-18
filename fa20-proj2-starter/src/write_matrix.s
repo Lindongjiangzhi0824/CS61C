327562fe26f7455e993bb2158447c1664ee2609b
Zhangyongjian <zhangyongjian20010824@gmail.com>
2025-10-18 12:15:28 +0800

    完成write_matrix.s函数与其单元测试

--- fa20-proj2-starter/src/write_matrix.s
+++ fa20-proj2-starter/src/write_matrix.s
@@ -25,16 +25,99 @@
 write_matrix:
 
     # Prologue
+    addi sp,sp,-32
+    sw ra,28(sp)
+    sw s0,24(sp)
+    sw s1,20(sp)
+    sw s2,16(sp)
+    sw s3,12(sp) 
+    sw s4,8(sp)  # 文件描述符
+    sw s5,4(sp)
 
+    mv s0,a0 # filename pointer
+    mv s1,a1 # matrix pointer
+    mv s2,a2 # rows data
+    mv s3,a3 # cols data
 
+    # open file
+    mv a1,s0
+    li a2,1 # a2:1-> write
+    jal fopen
 
+    li t0,-1
+    beq a0,t0,error_93
 
+    # save file descriptor to s3
+    mv s4,a0
 
+    # allocate space for rows and cols
+    addi sp,sp,-8
+    sw s2,0(sp) # rows
+    sw s3,4(sp) # cols
 
+    # write rows
+    mv a1,s4
+    # mv a2, s1 
+    # here buffer with matrix pointer is not right, should be written to row data rows and column data cols
 
+    mv a2,sp  # datasource : row (stack top)
+    li a3,1
+    li a4,4
 
+    jal fwrite
+    li t0,1
+    bne a0,t0,error_94
+
+    # write cols
+    mv a1,s4
+    addi a2,sp,4 # cols = stack top + 4
+    li a3,1
+    li a4,4
+
+    jal fwrite
+    li t0,1
+    bne a0,t0,error_94
+
+    addi sp,sp,8 # free temporary spaces
+
+    # calculate matrix elements number
+    mul s5,s2,s3
+    # each buffer size
+    li t0,4
+    # write matrix
+    mv a1,s4
+    mv a2,s1
+    mv a3,s5
+    mv a4,t0
+    jal fwrite
+    bne a0,s5,error_94
+
+    # close file
+    mv a1,s4
+    jal fclose
+    bne a0,zero,error_95
 
     # Epilogue
+    lw s5,4(sp)
+    lw s4,8(sp)
+    lw s3,12(sp)
+    lw s2,16(sp)
+    lw s1,20(sp)
+    lw s0,24(sp)
+    lw ra,28(sp)
+    addi sp,sp,32
 
 
     ret
+
+error_93:
+    li a1,93
+    j exit2
+
+error_94:
+    li a1,94
+    j exit2
+
+error_95:
+    li a1,95
+    j exit2
--- fa20-proj2-starter/unittests/unittests.py
+++ fa20-proj2-starter/unittests/unittests.py
@@ -233,17 +233,30 @@ class TestWriteMatrix(TestCase):
         # load output file name into a0 register
         t.input_write_filename("a0", outfile)
         # load input array and other arguments
-        raise NotImplementedError("TODO")
-        # TODO
+        matrix = t.array([1,2,3,4,5,6,7,8,9])
+        t.input_array("a1",matrix)
+        t.input_scalar("a2", 3)
+        t.input_scalar("a3", 3)
         # call `write_matrix` function
         t.call("write_matrix")
         # generate assembly and run it through venus
         t.execute(fail=fail, code=code)
         # compare the output file against the reference
-        t.check_file_output(outfile, "outputs/test_write_matrix/reference.bin")
+        # 如果 fail 字符串为空（没有 fail 错误）
+        if not fail:
+            t.check_file_output(outfile, "outputs/test_write_matrix/reference.bin")
 
     def test_simple(self):
         self.do_write_matrix()
+    
+    def test_exception_93(self):
+        self.do_write_matrix(fail='fopen', code=93)
+
+    def test_exception_94(self):
+        self.do_write_matrix(fail='fwrite', code=94)
+
+    def test_exception_95(self):
+        self.do_write_matrix(fail='fclose', code=95)
 
     @classmethod
     def tearDownClass(cls):
